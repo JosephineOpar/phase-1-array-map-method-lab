2ff33ad0fd8e69eee49252c76e0833670032c2ac
Josephine Opar <josephineopar@gmail.com>
2023-01-31 11:33:34 +0200

    Array map method

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(line => {
+    const wordsArray = line.split(' ')
+    const capitalizedWords = wordsArray.map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1)
+    })
+    return capitalizedWords.join(' ')
+  })
 }
